48453a21a7d78a0d6196415c010c37ba1d442dd9
Austin Hou <austhou@gmail.com>
2019-04-04 10:19:57 -0700

    splash

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import { reduxFirestore, firestoreReducer } from 'redux-firestore';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Helmet } from 'react-helmet';
 import ReduxThunk from 'redux-thunk';
 import firebase from 'firebase';
 import ReactGA from 'react-ga';
@@ -30,7 +31,7 @@ class App extends Component {
             this.setState({reactGaInitialized: true})
         }
         ReactGA.pageview(window.location.pathname + window.location.search);
-        
+        document.title = 'Albumlist'
     }
 
 
@@ -38,6 +39,24 @@ class App extends Component {
         return (
             <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
                 <div className="App">
+                    <Helmet>
+                        <meta name="title" content="Albumlist " />
+                        <meta name="description" content="Display what you've been listening to. " />
+
+                        
+                        <meta property="og:type" content="website" />
+                        <meta property="og:url" content="http://albumlist.co/" />
+                        <meta property="og:title" content="Albumlist " />
+                        <meta property="og:description" content="Showcase what you've been listening to. " />
+                        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+
+                        
+                        <meta property="twitter:card" content="summary_large_image" />
+                        <meta property="twitter:url" content="http://albumlist.co/" />
+                        <meta property="twitter:title" content="Albumlist " />
+                        <meta property="twitter:description" content="Showcase what you've been listening to. " />
+                        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+                    </Helmet>
                     <Router>
 
                         <Route
